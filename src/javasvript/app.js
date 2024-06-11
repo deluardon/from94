445429acbf440f10f7a869c96f7e4c0f610d7193
deluardon <160032360+deluardon@users.noreply.github.com>
2024-06-11 23:19:13 +0600

    first commit

---
+++ src/javasvript/app.js
@@ -0,0 +1,11 @@
+let popup=document.getElementById("popup");
+
+let close=document.getElementById("close");
+function popupOpen(){
+    popup.classList.add("popup-open");
+}
+function popupClose(){
+    popup.classList.remove("popup-open");
+}
+
+
